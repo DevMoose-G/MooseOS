1789ee8f5658f432b034e3252bf7b9723fb3b9dc
Muse Gimicael <muse.gimicael@gmail.com>
2022-05-20 00:01:29 -0500

    Created OUT and INB Functions in Assembly and Created the Updated Cursor Function

--- func.asm
+++ func.asm
@@ -1,39 +1,20 @@
+global outb
+global inb
 global cursor_enable
-;type cursor_enable,%function
-cursor_enable:
-	; r0 is cursor start
-	; r1 is cursor end
-	; stores the flags and registers on the stack
-	;pushf
 
-	mov bh, [esp+4] ; takes the first int
-	mov bl, [esp+8] ; takes the second int
-	
-	; outb(0x3D4, 0x0A)
-	mov dx, 0x3D4
-	mov al, 0x0A
+; outputs al to the port dx
+outb: ; (dx, al)
+
+	; dx is the port
+	; al is the data fed to the port
+	mov dx, [esp+4]
+	mov al, [esp+8]
 	out dx, al
+	ret
 
-	; outb(0x3D5, (inb(0x3D5) & 0xC0) | cursor_start)
-	mov dx, 0x3D5
-	in al, dx
-	;mov al, [es*16+di]
-	and al, 0xC0
-	or al, bh
-	out dx, al
-
-	; outb(0x3D4, 0x0B)
-	mov dx, 0x3D4
-	mov al, 0x0B
-	out dx, al
-
-	; outb(0x3D5, (inb(0x3D5) & 0xE0) | cursor_end)
-	mov dx, 0x3D5
-	in al, dx
-	;mov al, [es*16+di]
-	and al, 0xE0
-	or al, bl
-	out dx, al 
-
+; returns the data at port dx
+inb: ; (dx) 
+	mov dx, [esp+4]
+	in eax, dx ; eax is where you store what you return 
 	ret
 
--- kernel.c
+++ kernel.c
@@ -1,4 +1,17 @@
-extern void cursor_enable(int cursor_start, int cursor_end);
+#define VGA_WIDTH 80
+#define VIDEO_START 0xb8000
+
+extern void outb(int port, int data);
+extern int inb(int port);
+
+void update_cursor(int x, int y){
+	int pos = y * VGA_WIDTH + x;
+	
+	outb(0x3D4, 0x0F);
+	outb(0x3D5, (int) (pos & 0xFF));
+	outb(0x3D4, 0x0E);
+	outb(0x3D5, (int) ((pos >> 8) & 0xFF));
+}
 
 int printAt(const char* str, int vidAddress){
 	while(*str != '\0'){
@@ -6,12 +19,25 @@ int printAt(const char* str, int vidAddress){
 		vidAddress+=2;
 		str++;
 	}
+	
+	// updates cursor
+	update_cursor((vidAddress - VIDEO_START)/2, 0);
 	return vidAddress;
 }
 
+
+void cursor_enable(int cursor_start, int cursor_end){
+	outb(0x3D4, 0x0A);
+	outb(0x3D5, (inb(0x3D5) & 0xC0) | cursor_start);
+	
+	outb(0x3D4, 0x0B);
+	outb(0x3D5, (inb(0x3D5) & 0xE0) | cursor_end); 
+}
+
+
 extern void main(){
 	int vidAddress = 0xb8000;
-	cursor_enable(0, 15);
+	cursor_enable(0, 25);
 	vidAddress = printAt("Damn this works! ", vidAddress);	
 	vidAddress = printAt("I can write multiple lines from C to here. ", vidAddress);
 }
